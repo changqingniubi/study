2f724fcfbe7c5e7d04c210f246597565abce44c4
changqing <qing.chang@eeoa.com>
2022-10-16 20:49:25 +0800

    antd-form

---
+++ React/antd-form/src/components/Form/index.tsx
@@ -0,0 +1,17 @@
+import InternalForm from "./Form";
+import Field from "./Field";
+import useForm from "./useForm";
+
+type InternalFormType = typeof InternalForm;
+
+interface FormInterface extends InternalFormType {
+  useForm: typeof useForm;
+  Field: typeof Field;
+}
+
+const Form = InternalForm as FormInterface;
+Form.useForm = useForm;
+Form.Field = Field;
+
+export { Field, useForm };
+export default Form;
